1f1fa7b9c991393a64816893c232753854479e34
Sikaka <36932404+Sikaka@users.noreply.github.com>
2018-03-01 11:27:31 -0500

    Velkoz added
    Added initial Velkoz support (W Detonate passive, E interupt gapcloser/hourglass). Does not support Q yet.

--- Auto.lua
+++ Auto.lua
@@ -1,10 +1,15 @@
-local Heroes = {"Nami","Brand"}
-if not table.contains(Heroes, myHero.charName) then return end
+local Heroes = {"Nami","Brand", "Velkoz"}
+if not table.contains(Heroes, myHero.charName) then print("Hero not supported: " .. myHero.charName) return end
 
 local Scriptname,Version,Author,LVersion = "[Auto]","v1.0","Sikaka","0.01"
 
 
-Callback.Add("Load",function() _G[myHero.charName]() end)
+Callback.Add("Load",
+function() 
+	_G[myHero.charName]() 
+	AutoUtil()
+	TPred()
+end)
  	
 
 function CurrentPctLife(entity)
@@ -61,6 +66,29 @@ function IsRecalling()
 	return false
 end
 
+class "AutoUtil"
+function AutoUtil:GetDistanceSqr(p1, p2)
+	assert(p1, "GetDistance: invalid argument: cannot calculate distance to "..type(p1))
+	assert(p2, "GetDistance: invalid argument: cannot calculate distance to "..type(p2))
+	return (p1.x - p2.x) ^ 2 + ((p1.z or p1.y) - (p2.z or p2.y)) ^ 2
+end
+
+function AutoUtil:GetDistance(p1, p2)
+	return math.sqrt(self:GetDistanceSqr(p1, p2))
+end
+function AutoUtil:NearestEnemyDistance(entity)
+	local distance = 999999
+	for i = 1,Game.HeroCount()  do
+		local hero = Game.Hero(i)	
+		if isValidTarget(hero,range) and hero.team ~= myHero.team then
+			local d = self:GetDistance(entity.pos, hero.pos)
+			if d < distance then
+				distance = d
+			end
+		end
+	end
+	return distance
+end
 
 class "Brand"
 function Brand:__init()	
@@ -71,14 +99,13 @@ function Brand:__init()
 	Callback.Add("Draw", function() self:Draw() end)
 end
 function Brand:LoadSpells()
-	Q = {Range = 1000, Width = 80, Delay = 0.30, Speed = 1550, Collision = true, aoe = false, Sort = 'line'}
-	W = {Range = 900, Width = 240, Delay = 0.75, Speed = 99999, Collision = false, aoe = true, Sort = "circular"}
-	E = {Range = 600, Delay = 0.25, Speed = 99999, Collision = false }
+	Q = {Range = 1050, Width = 80, Delay = 0.25, Speed = 1550, Collision = true, aoe = false, Sort = 'line'}
+	W = {Range = 900, Width = 250, Delay = 0.625, Speed = math.huge, Collision = false, aoe = true, Sort = "circular"}
+	E = {Range = 600, Delay = 0.25, Speed = math.huge, Collision = false }
 	R = {Range = 750, Width = 0, Delay = 0.25, Speed = 1700, Collision = false, aoe = false, Sort = "circular"}
 end
 
 function Brand:CreateMenu()
-	TPred()
 	
 	AIO = MenuElement({type = MENU, id = myHero.charName, name = "[Auto] "..myHero.charName})
 	AIO:MenuElement({id = "Skills", name = "Skills", type = MENU})
@@ -156,13 +183,116 @@ function Brand:Tick()
 	end
 end
 
-class "Nami"
+class "Velkoz"
 
+function Velkoz:__init()	
+	print("Loaded [Auto] "..myHero.charName)
+	self:LoadSpells()
+	self:CreateMenu()
+	Callback.Add("Tick", function() self:Tick() end)
+	Callback.Add("Draw", function() self:Draw() end)
+end
+
+function Velkoz:LoadSpells()
+
+	Q = {Range = 1050, Width = 55,Delay = 0.251, Speed = 1235,  Sort = "line"}
+	W = {Range = 1050, Width = 80,Delay = 0.25, Speed = 1500,  Sort = "line"}
+	E = {Range = 850, Width = 235,Delay = 0.75, Speed = math.huge,  Sort = "circular"}
+	R = {Range = 1550,Width = 75, Delay = 0.25, Speed = math.huge, Sort = "line" }
+end
+
+function Velkoz:CreateMenu()
+	AIO = MenuElement({type = MENU, id = myHero.charName, name = "[Auto] " .. myHero.charName})
+	
+	AIO:MenuElement({id = "Skills", name = "Skills", type = MENU})
+	
+	--%Mana needed for us to launch a Q vs immobile targets
+	AIO.Skills:MenuElement({id = "QMana", name = "Auto Q Mana", value = 25, min = 5, max = 100, step = 1 })
+	
+	--%Mana needed for us to use W to detonate passive or steal a kill
+	AIO.Skills:MenuElement({id = "WDetonateMana", name = "W Mana", value = 50, min = 5, max = 100, step = 5 })
+	
+	--Timing accuracy (seconds) for our E to land after hourglass/dash
+	AIO.Skills:MenuElement({id = "ETiming", name = "E Interupt Delay", value = .25, min = .1, max = 1, step = .05 })	
+	
+	--Minimum E mana to use on stunned targets
+	AIO.Skills:MenuElement({id = "EMana", name = "Auto E Mana", value = 25, min = 5, max = 100, step = 1 })	
+	
+	AIO:MenuElement({id = "autoSkillsActive", name = "Auto Skills Enabled",value = true, toggle = true, key = 0x7A })
+end
+
+function Velkoz:Draw()
+	if AIO.autoSkillsActive:Value() then
+		local textPos = myHero.pos:To2D()
+		Draw.Text("ON", 20, textPos.x - 25, textPos.y + 40, Draw.Color(220, 0, 255, 0))
+	end
+	
+	for i = 1, Game.HeroCount() do
+		local Hero = Game.Hero(i)    
+		if Hero.isEnemy and Hero.pathing.hasMovePath and Hero.pathing.isDashing and Hero.pathing.dashSpeed>500 then
+			Draw.Circle(Hero:GetPath(1), 40, 20, Draw.Color(255, 255, 255, 255))
+		end
+	end
+end
+
+function Velkoz:Tick()
+	if myHero.dead or Game.IsChatOpen() == true or IsRecalling() == true or not AIO.autoSkillsActive:Value() then return end
+	
+	if Ready(_E) then 
+		self:AutoEInterupt()
+	end
+	
+	if Ready(_W) then
+		self:AutoWDetonate()
+	end
+end
+
+
+function Velkoz:FindEnemyWithBuff(buffName, range, stackCount)
+	for i = 1, Game.HeroCount() do
+		local Hero = Game.Hero(i)    
+		if Hero.isEnemy and AutoUtil:GetDistance(myHero.pos, Hero.pos) <= range then
+			for bi = 1, Hero.buffCount do 
+			local Buff = Hero:GetBuff(bi)
+				if Buff.name == buffName and Buff.duration > 0 and Buff.count >= stackCount then
+					return Hero
+				end
+			end
+		end
+	end
+end
+
+function Velkoz:AutoEInterupt()
+	--Use E to target the end of a gapcloser
+	local target = TPred:GetInteruptTarget(myHero.pos, E.Range, E.Delay, E.Speed, AIO.Skills.ETiming:Value())
+	if target ~= nil then
+		Control.CastSpell(HK_E, target:GetPath(1))
+	end
+	
+	--Use E to target the end of a hourglass stasis
+	local target = TPred:GetStasisTarget(myHero.pos, E.Range, E.Delay, E.Speed, AIO.Skills.ETiming:Value())
+	if target ~= nil then
+		Control.CastSpell(HK_E, target.pos)	
+	end	
+end
+
+
+--Find an enemy with 2 stacks of passive on them and use W to pop it.
+function Velkoz:AutoWDetonate()
+	local Enemy = self:FindEnemyWithBuff("velkozresearchstack", W.Range, 2)
+	if Enemy ~= nil then	
+		local castpos,HitChance, pos = TPred:GetBestCastPosition(Enemy, W.Delay , W.Width, W.Range, W.Speed, myHero.pos, W.Collision, W.Sort)
+		if HitChance >= 2 then
+			Control.CastSpell(HK_W, castpos)
+		end
+	end
+end
+
+class "Nami"
 local _adcHeroes = { "Ashe", "Caitlyn", "Corki", "Draven", "Ezreal", "Graves", "Jhin", "Jinx", "Kalista", "KogMaw", "Lucian", "MissFortune", "Quinn", "Sivir", "Teemo", "Tristiana", "Twitch", "Varus", "Vayne", "Xayah"}
 	
 function Nami:__init()	
 	print("Loaded [Auto] "..myHero.charName)
-	TPred()	
 	self:LoadSpells()
 	self:CreateMenu()
 	Callback.Add("Tick", function() self:Tick() end)
@@ -170,14 +300,14 @@ function Nami:__init()
 end
 function Nami:LoadSpells()
 
-	Q = {Range = 875, Width = 150,Delay = 0.75, Speed = 2000,  Sort = "circular"}
+	Q = {Range = 875, Width = 200,Delay = 0.95, Speed = math.huge,  Sort = "circular"}
 	W = {Range = 725}
 	E = { Range = 800}
-	R = {Range = 2750,Width = 260, Speed = 850, Delay = 0.5, Sort = "line" }
+	R = {Range = 2750,Width = 215, Speed = 850, Delay = 0.5, Sort = "line" }
 end
 
 function Nami:CreateMenu()
-	AIO = MenuElement({type = MENU, id = "Nami", name = "[Auto] " .. myHero.charName})
+	AIO = MenuElement({type = MENU, id = myHero.charName, name = "[Auto] " .. myHero.charName})
 	
 	AIO:MenuElement({id = "Skills", name = "Skills", type = MENU})
 	AIO.Skills:MenuElement({id = "QTiming", name = "Q Interupt Delay", value = .25, min = .1, max = 1, step = .05 })
@@ -234,28 +364,6 @@ function Nami:Tick()
 	self:AutoCrucible()
 end
 
-function Nami:GetDistanceSqr(p1, p2)
-	assert(p1, "GetDistance: invalid argument: cannot calculate distance to "..type(p1))
-	assert(p2, "GetDistance: invalid argument: cannot calculate distance to "..type(p2))
-	return (p1.x - p2.x) ^ 2 + ((p1.z or p1.y) - (p2.z or p2.y)) ^ 2
-end
-
-function Nami:GetDistance(p1, p2)
-	return math.sqrt(self:GetDistanceSqr(p1, p2))
-end
-function Nami:NearestEnemyDistance(entity)
-	local distance = 999999
-	for i = 1,Game.HeroCount()  do
-		local hero = Game.Hero(i)	
-		if isValidTarget(hero,range) and hero.team ~= myHero.team then
-			local d = self:GetDistance(entity.pos, hero.pos)
-			if d < distance then
-				distance = d
-			end
-		end
-	end
-	return distance
-end
 
 function Nami:AutoQInterupt()
 	--Use Q to target the end of a gapcloser
@@ -274,7 +382,7 @@ end
 function Nami:AutoWEmergency()
 	for i = 1, Game.HeroCount() do
 		local Hero = Game.Hero(i)
-		if Hero.isAlly and self:GetDistance(myHero.pos, Hero.pos) <= W.Range and CurrentPctLife(Hero) <= AIO.Skills.WEmergencyPct:Value() then
+		if Hero.isAlly and AutoUtil:GetDistance(myHero.pos, Hero.pos) <= W.Range and CurrentPctLife(Hero) <= AIO.Skills.WEmergencyPct:Value() then
 			Control.CastSpell(HK_W, Hero.pos)			
 		end
 	end
@@ -283,8 +391,8 @@ end
 function Nami:AutoWBounce()
 	for i = 1, Game.HeroCount() do
 		local Hero = Game.Hero(i)
-		if Hero.isAlly and self:GetDistance(myHero.pos, Hero.pos) <= W.Range and CurrentPctLife(Hero) <= AIO.Skills.WBouncePct:Value() then
-			if self:NearestEnemyDistance(Hero) < 500 then
+		if Hero.isAlly and AutoUtil:GetDistance(myHero.pos, Hero.pos) <= W.Range and CurrentPctLife(Hero) <= AIO.Skills.WBouncePct:Value() then
+			if AutoUtil:NearestEnemyDistance(Hero) < 500 then
 				Control.CastSpell(HK_W, Hero.pos)
 			end
 		end
@@ -294,12 +402,13 @@ end
 function Nami:AutoE()
 	for i = 1, Game.HeroCount() do
 		local Hero = Game.Hero(i)
-		if  Hero.isAlly and Hero ~= myHero and self:GetDistance(myHero.pos, Hero.pos) <= E.Range then
-			--Check if they are using an auto attack spell
+		if  Hero.isAlly and Hero ~= myHero and AutoUtil:GetDistance(myHero.pos, Hero.pos) <= E.Range and table.contains(_adcHeroes, Hero.charName) then
+			--Check for targeted spells on enemies. Useful for caster ADCs
 			local targetHandle = nil			
-			if Hero.activeSpell and Hero.activeSpell.valid and Hero.activeSpell.isAutoAttack and Hero.activeSpell.target then
+			if Hero.activeSpell and Hero.activeSpell.valid and Hero.activeSpell.target then
 				targetHandle = Hero.activeSpell.target
 			end
+			--Check for a pure auto attack with no modifiers (shouldn't be needed but havent tested fully without it. Remove later)
 			if Hero.attackData and Hero.attackData.state == STATE_WINDUP and Hero.attackData.target then
 				targetHandle = Hero.attackData.target
 			end
